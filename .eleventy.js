e5103520e3b82a5da2598d75c8a3d50aad51a986
Blake Johnson <blake.k.johnson.4@gmail.com>
2024-12-26 14:12:14 -0500

    add the interesting links page

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (config) {
   config.addPassthroughCopy("bundle.css");
   config.addPassthroughCopy("assets");
+  config.addFilter("toJSON", (arr) => {
+    return JSON.stringify(arr);
+  });
 };
 
